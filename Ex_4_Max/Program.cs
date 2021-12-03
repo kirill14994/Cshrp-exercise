5f3c5287c5412baf3719ceb0c94b34888d586d1a
kirill14994 <94280573+kirill14994@users.noreply.github.com>
2021-12-03 19:35:56 +0300

    add ex 4

---
+++ Ex_4_Max/Program.cs
@@ -0,0 +1,38 @@
+﻿// Найти максимальное из трех чисел
+
+Console.Write("Введите число 1: ");
+
+int numA = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число 2: ");
+
+int numB = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число 3: ");
+
+int numC = int.Parse(Console.ReadLine());
+
+int max = numA;
+
+if (numA < numB)
+{
+    max = numB;
+}
+
+else
+{
+    max = numA;
+}
+
+if (max < numC)
+{
+    max = numC;
+}
+
+else 
+{
+    max = numA;
+}
+
+Console.Write("Максимальное: ");
+Console.Write(max);
