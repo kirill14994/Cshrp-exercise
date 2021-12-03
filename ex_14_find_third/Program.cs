f8aaea63f6c43fdf9e3446d2d2ac356087443399
kirill14994 <94280573+kirill14994@users.noreply.github.com>
2021-12-04 00:08:18 +0300

    add 12,13

---
+++ ex_14_find_third/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+
+Console.Write("Введиет число: ");
+int number = int.Parse(Console.ReadLine());
+
+
+
+if (number > 100)
+{
+    int last = number % 10;
+    Console.WriteLine(last);
+}
+
+else
+{
+    Console.WriteLine("нет");
+}
