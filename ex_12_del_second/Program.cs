187c895be16f3d1b2a5c50d683833def7d8cfc7e
kirill14994 <94280573+kirill14994@users.noreply.github.com>
2021-12-04 00:09:40 +0300

    add 12,13,14

---
+++ ex_12_del_second/Program.cs
@@ -0,0 +1,20 @@
+﻿// Удалить вторую цифру трёхзначного числа
+
+Console.Write("Введиет число: ");
+int number = int.Parse(Console.ReadLine());
+
+
+
+if (number <= 999 && number >= 100)
+{
+    int last = number % 10;
+    int first = number / 100;
+    int final = first * 10 + last;
+
+    Console.WriteLine(final);
+}
+
+else
+{
+    Console.WriteLine("число не трехзначное");
+}
---
+++ ex_13_multiplicity/Program.cs
@@ -0,0 +1,22 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+
+Console.Write("Введиет число: ");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.Write("Число которому должно быть кратно: ");
+int number2 = int.Parse(Console.ReadLine());
+
+int result = number1 % number2;
+
+if (result == 0)
+{
+    Console.WriteLine("Кратно");
+}
+
+else
+{
+    Console.WriteLine(result);
+}
+
+
