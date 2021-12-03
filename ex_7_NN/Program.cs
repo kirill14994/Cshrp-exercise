4fc2fc4378c36573881876255cacac455d64f793
kirill14994 <94280573+kirill14994@users.noreply.github.com>
2021-12-03 20:13:12 +0300

    add 5,6,7

---
+++ ex_7_NN/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать числа от -N до N
+
+Console.Write("Введиет число N: ");
+int N = int.Parse(Console.ReadLine());
+
+int minusN = -N;
+
+while (minusN <= N)
+{
+    Console.WriteLine(minusN);
+    minusN = minusN +1;
+}
